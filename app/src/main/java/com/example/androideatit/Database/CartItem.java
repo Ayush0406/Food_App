50c3ef91b152efcc3f9b5b188f56e5bf07b694c1
sahej02 <sahejveer87@gmail.com>
2020-04-12 22:00:21 +0530

    Database added for cart

--- app/src/main/java/com/example/androideatit/Database/CartItem.java
+++ app/src/main/java/com/example/androideatit/Database/CartItem.java
@@ -1,4 +1,122 @@
 package com.example.androideatit.Database;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "Cart")
 public class CartItem {
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "foodId")
+    private String foodId;
+
+    @ColumnInfo(name = "foodName")
+    private String foodName;
+
+    @ColumnInfo(name = "foodImage")
+    private String foodImage;
+
+    @ColumnInfo(name = "foodPrice")
+    private Double foodPrice;
+
+    @ColumnInfo(name = "foodQuantity")
+    private Double foodQuantity;
+
+    @ColumnInfo(name = "userPhone")
+    private String userPhone;
+
+    @ColumnInfo(name = "foodExtraPrice")
+    private Double foodExtraPrice;
+
+    @ColumnInfo(name = "foodAddOn")
+    private String foodAddOn;
+
+    @ColumnInfo(name = "foodSize")
+    private Double foodSize;
+
+    @ColumnInfo(name = "uid")
+    private Double uid;
+
+    @NonNull
+    public String getFoodId() {
+        return foodId;
+    }
+
+    public void setFoodId(@NonNull String foodId) {
+        this.foodId = foodId;
+    }
+
+    public String getFoodName() {
+        return foodName;
+    }
+
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+
+    public String getFoodImage() {
+        return foodImage;
+    }
+
+    public void setFoodImage(String foodImage) {
+        this.foodImage = foodImage;
+    }
+
+    public Double getFoodPrice() {
+        return foodPrice;
+    }
+
+    public void setFoodPrice(Double foodPrice) {
+        this.foodPrice = foodPrice;
+    }
+
+    public Double getFoodQuantity() {
+        return foodQuantity;
+    }
+
+    public void setFoodQuantity(Double foodQuantity) {
+        this.foodQuantity = foodQuantity;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public Double getFoodExtraPrice() {
+        return foodExtraPrice;
+    }
+
+    public void setFoodExtraPrice(Double foodExtraPrice) {
+        this.foodExtraPrice = foodExtraPrice;
+    }
+
+    public String getFoodAddOn() {
+        return foodAddOn;
+    }
+
+    public void setFoodAddOn(String foodAddOn) {
+        this.foodAddOn = foodAddOn;
+    }
+
+    public Double getFoodSize() {
+        return foodSize;
+    }
+
+    public void setFoodSize(Double foodSize) {
+        this.foodSize = foodSize;
+    }
+
+    public Double getUid() {
+        return uid;
+    }
+
+    public void setUid(Double uid) {
+        this.uid = uid;
+    }
 }
