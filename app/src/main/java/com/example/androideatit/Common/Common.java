7b987726fc40610bd45e4d8a5bdc06527c90c9e7
Ayush0406 <laddhaayush@gmail.com>
2020-04-14 11:57:23 +0530

    Minor changes in food list layout and Common class

--- app/src/main/java/com/example/androideatit/Common/Common.java
+++ app/src/main/java/com/example/androideatit/Common/Common.java
@@ -1,7 +1,9 @@
 package com.example.androideatit.Common;
 
 import Model.CategoryModel;
+import Model.User;
 
 public class Common {
     public static CategoryModel categorySelected;
+    public static User currentUser;
 }
