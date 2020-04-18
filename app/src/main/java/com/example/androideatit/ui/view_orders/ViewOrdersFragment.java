7b7a0b88913add5f981b9140859fae8c503480b9
Burk <mohammedburk@gmail.com>
2020-04-18 05:56:00 +0530

    Started work on View Orders

--- app/src/main/java/com/example/androideatit/ui/view_orders/ViewOrdersFragment.java
+++ app/src/main/java/com/example/androideatit/ui/view_orders/ViewOrdersFragment.java
@@ -8,11 +8,30 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.androideatit.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 public class ViewOrdersFragment extends Fragment {
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+
+    @BindView(R.id.recycler_orders)
+    RecyclerView recycler_orders;
+
+    private Unbinder unbinder;
+
+    private ViewOrdersViewModel toolsViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+//        toolsViewModel =
+//                ViewModelProviders.of(this).get(ViewOrdersViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_view_order, container, false);
+//        unbinder = ButterKnife.bind(this.root);
+        return root;
     }
 }
