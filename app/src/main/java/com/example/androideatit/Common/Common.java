f0810b5cb37311a16f028966db4385a6ab746aca
Ayush0406 <laddhaayush@gmail.com>
2020-04-19 20:37:17 +0530

    Added delivery person view orders, call and remove order fucntionality. Sign In pending (Updated file names 2)

--- app/src/main/java/com/example/androideatit/Common/Common.java
+++ app/src/main/java/com/example/androideatit/Common/Common.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.util.List;
+import java.util.Random;
 
 import Model.AddonModel;
 import Model.CategoryModel;
@@ -68,6 +69,51 @@ public class Common {
         }
 
 
+    }
+
+    public static String createOrderNumber() {
+        return new StringBuilder()
+                .append(System.currentTimeMillis()) //current time in milliseconds
+                .append(Math.abs(new Random().nextInt())) //random number to block same order at same time
+                .toString();
+    }
+
+    public static String getDateofWeek(int i) {
+        switch (i)
+        {
+            case 1:
+                return "Monday";
+            case 2:
+                return "Tuesday";
+            case 3:
+                return "Wednesday";
+            case 4:
+                return "Thursday";
+            case 5:
+                return "Friday";
+            case 6:
+                return "Saturday";
+            case 7:
+                return "Sunday";
+            default:
+                return "unknown";
+        }
+    }
+
+    public static String convertStatusToText(int orderStatus) {
+        switch (orderStatus)
+        {
+            case 0:
+                return "Placed";
+            case 1:
+                return "Shipping";
+            case 2:
+                return "Shipped";
+            case 3:
+                return "Cancelled";
+            default:
+                return "unknown";
+        }
     }
 
 //    public static void setSpanStringColor(String order_date_, String format, TextView txt_time, int color)
