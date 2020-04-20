e4c2a8c810aa48aaa2be39bc3e5bad6f0e8b06d2
Ayush0406 <laddhaayush@gmail.com>
2020-04-20 10:43:37 +0530

    Delivery end order update features completed

--- app/src/main/java/com/example/androideatit/Delivery/order/OrderFragment.java
+++ app/src/main/java/com/example/androideatit/Delivery/order/OrderFragment.java
@@ -7,6 +7,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -17,14 +18,20 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.DisplayMetrics;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LayoutAnimationController;
+import android.widget.Adapter;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.androideatit.Adapter.MyOrderAdapter_d;
+import com.example.androideatit.Common.Common;
 import com.example.androideatit.Common.MySwipeHelper;
 import com.example.androideatit.R;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -37,7 +44,9 @@ import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import Model.OrderModel;
 import butterknife.BindView;
@@ -90,8 +99,31 @@ public class OrderFragment extends Fragment {
         {
             @Override
             public void instantiateMyButton(RecyclerView.ViewHolder viewHolder, List<MyButton> buf) {
-                buf.add(new MyButton(getContext(), "Directions", 30, 0, Color.parseColor("#9b0000"), pos->{
-
+                buf.add(new MyButton(getContext(), "Accept", 30, 0, Color.parseColor("#9b0000"), pos->{
+                    OrderModel orderModel = adapter.getItemAtPosition(pos);
+                    if(orderModel.getOrderStatus() == 1)
+                    {
+                        Toast.makeText(getContext(), "Order already accepted.", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Map<String, Object> updateData = new HashMap<>();
+                        updateData.put("orderStatus", 1);
+                        FirebaseDatabase.getInstance().getReference("Orders").child(orderModel.getKey())
+                                .updateChildren(updateData)
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(getContext(), ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                adapter.updateItemStatusAtPosition(pos, 1);
+                                adapter.notifyItemChanged(pos);
+                                Toast.makeText(getContext(), "Order Update Successful.", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
                 }));
 
                 buf.add(new MyButton(getContext(), "Call", 30, 0, Color.parseColor("#560027"), pos->{
@@ -117,11 +149,10 @@ public class OrderFragment extends Fragment {
                     }).check();
                 }));
 
-
-                buf.add(new MyButton(getContext(), "Remove", 30, 0, Color.parseColor("#12005e"), pos -> {
+                buf.add(new MyButton(getContext(), "Reject", 30, 0, Color.parseColor("#12005e"), pos -> {
                     AlertDialog.Builder builder = new AlertDialog.Builder(getContext()).setTitle("Delete Order")
-                            .setMessage("Do you really want to delete this order?")
-                            .setNegativeButton("CANCEL", (dialog, which) -> dialog.dismiss()).setPositiveButton("DELETE", new DialogInterface.OnClickListener() {
+                            .setMessage("Do you really want to reject this order?")
+                            .setNegativeButton("CANCEL", (dialog, which) -> dialog.dismiss()).setPositiveButton("REJECT", new DialogInterface.OnClickListener() {
                                 @Override
                                 public void onClick(DialogInterface dialog, int which) {
                                     OrderModel orderModel = adapter.getItemAtPosition(pos);
@@ -149,11 +180,104 @@ public class OrderFragment extends Fragment {
                     dialog.getButton(DialogInterface.BUTTON_POSITIVE).setTextColor(Color.RED);
                 }));
 
-                buf.add(new MyButton(getContext(), "Edit", 30, 0, Color.parseColor("#336699"), pos->{
+                buf.add(new MyButton(getContext(), "Fulfilled", 30, 0, Color.parseColor("#336699"), pos->{
+//                    showEditDialog(adapter.getItemAtPosition(pos), pos);
 
+                    OrderModel orderModel = adapter.getItemAtPosition(pos);
+                    if(orderModel.getOrderStatus() == 2)
+                    {
+                        Toast.makeText(getContext(), "Order already completed.", Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        FirebaseDatabase.getInstance().getReference("Orders").child(orderModel.getKey())
+                                .removeValue()
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(getContext(), ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                adapter.removeItem(pos);
+                                adapter.notifyItemRemoved(pos);
+                                Toast.makeText(getContext(), "Order Status Successfully Updated.", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
                 }));
             }
         };
     }
+
+//    private void showEditDialog(OrderModel orderModel, int pos) {
+//        View layout_dialog;
+//        AlertDialog.Builder builder;
+//        if(orderModel.getOrderStatus() == 0) //pending
+//        {
+//            layout_dialog = LayoutInflater.from(getContext()).inflate(R.layout.layout_dialog_shipping, null);
+//            builder = new AlertDialog.Builder(getContext(), android.R.style.Theme_Material_Light_NoActionBar_Fullscreen).setView(layout_dialog);
+//        }
+//        else if(orderModel.getOrderStatus() == -1) //cancelled
+//        {
+//            layout_dialog = LayoutInflater.from(getContext()).inflate(R.layout.layout_dialog_cancelled, null);
+//            builder = new AlertDialog.Builder(getContext()).setView(layout_dialog);
+//        }
+//        else
+//        {
+//            layout_dialog = LayoutInflater.from(getContext()).inflate(R.layout.layout_dialog_shipped, null);
+//            builder = new AlertDialog.Builder(getContext()).setView(layout_dialog);
+//        }
+//
+//        Button btn_ok = (Button)layout_dialog.findViewById(R.id.btn_ok_d);
+//        Button btn_cancel = (Button)layout_dialog.findViewById(R.id.btn_cancel_d);
+//
+//        RadioButton rdi_shipping = (RadioButton)layout_dialog.findViewById(R.id.rdi_shipping_d);
+//        RadioButton rdi_shipped = (RadioButton)layout_dialog.findViewById(R.id.rdi_shipped_d);
+//        RadioButton rdi_cancelled = (RadioButton)layout_dialog.findViewById(R.id.rdi_cancelled_d);
+//        RadioButton rdi_delete = (RadioButton)layout_dialog.findViewById(R.id.rdi_delete_d);
+//        RadioButton rdi_restore_placed = (RadioButton)layout_dialog.findViewById(R.id.rdi_restore_placed_d);
+//
+//        //set Data
+//        TextView txt_status = (TextView)layout_dialog.findViewById(R.id.txt_status_d);
+//        txt_status.setText(new StringBuilder("Order Staus(").append(Common.convertStatusToString_d(orderModel.getOrderStatus())));
+//
+//        //Create Dialog
+//        AlertDialog dialog = builder.create();
+//        dialog.show();
+//        //Adding customizations to the dialog
+//        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+//        dialog.getWindow().setGravity(Gravity.CENTER);
+//
+//        btn_cancel.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                dialog.dismiss();
+//            }
+//        });
+//
+//        btn_ok.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                dialog.dismiss();
+//                if(rdi_cancelled != null && rdi_cancelled.isChecked())
+//                    updateOrder(pos, orderModel, -1);
+//                else if(rdi_shipping != null && rdi_shipping.isChecked())
+//                    updateOrder(pos, orderModel, 1);
+//                else if(rdi_shipped != null && rdi_shipped.isChecked())
+//                    updateOrder(pos, orderModel, 2);
+//                else if(rdi_restore_placed != null && rdi_restore_placed.isChecked())
+//                    updateOrder(pos, orderModel, 0);
+//                else if(rdi_delete != null && rdi_delete.isChecked())
+//                    deleteOrder(pos, orderModel);
+//            }
+//        });
+//    }
+//
+//    private void updateOrder(int pos, OrderModel orderModel, int status)
+//    {
+//
+//    }
 
 }
